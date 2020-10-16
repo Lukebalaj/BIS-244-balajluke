18b8dfe2a48a63613e388842fbb1391c48b306a1
Luke Balaj <Ldb222@lehigh.edu>
2020-10-16 17:16:03 -0400

    Midterm Exam

---
+++ Midterm Exam.R
@@ -0,0 +1,25 @@
+#Luke Balaj
+#Set WD
+setwd("~/Documents/BIS244/BIS-244-balajluke")
+
+#library
+library(ggplot2)
+library(readr)
+library(tidyverse)
+library(dplyr)
+library(scales)
+
+#read
+covid <- read.csv("data.csv")
+
+#cleaning up and filtering data
+newdata <- covid %>% filter(countriesAndTerritories == "Canada" | countriesAndTerritories == "France" | countriesAndTerritories =="Germany" | countriesAndTerritories =="Italy" | countriesAndTerritories == "Japan" | countriesAndTerritories == "United_Kingdom" | countriesAndTerritories == "United_States_of_America")
+newdata$dateRep <- as.Date(newdata$dateRep, "%d/%m/%y")
+
+#plotting data on line graph
+p <- ggplot(data = newdata, mapping = aes(x = dateRep , y = cases, color = countriesAndTerritories))
+p + geom_line(mapping = aes(group = countriesAndTerritories)) +
+  labs(x = NULL, y = "New Cases per Day", title = "G7 COVID-19 Case Counts",
+       subtitle = "by Luke Balaj as of 2020-10-15",
+       caption = "Data: ECDC Times") +
+  scale_x_date(date_labels = "%b")
